194fc162c8b9cceda4e8c5d3aecb3dade62beab2
Santosh-k12 <pagolusantoshk@gmail.com>
2024-04-24 03:22:37 +0530

    drew polygon using custom paint

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math' show pi, cos, sin;
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,9 +31,66 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class Polygon extends CustomPainter {
+  final int sides;
+
+  Polygon({
+    required this.sides,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.blue
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 3;
+
+    final path = Path();
+
+    final center = Offset(size.width / 2, size.width / 2);
+
+    final angle = 2 * pi / sides;
+
+    final angles = List.generate(sides, (index) => index * angle);
+
+    final radius = size.width / 2;
+
+    path.moveTo(
+      center.dx + radius * cos(0),
+      center.dy + radius * sin(0),
+    );
+
+    for (final angle in angles) {
+      path.lineTo(
+        center.dx + radius * cos(angle),
+        center.dy + radius * sin(angle),
+      );
+    }
+
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) =>
+      oldDelegate is Polygon && oldDelegate.sides != sides;
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: CustomPaint(
+          painter: Polygon(sides: 10),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width,
+          ),
+        ),
+      ),
+    );
   }
 }
